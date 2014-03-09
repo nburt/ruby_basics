829bf3d0565c09285f89a2f79aa7ac92c31c1353
Nathanael Burt <nathanael.burt@gmail.com>
2014-03-09 14:21:37 -0600

    handles the numbers that are just below the next boundary

--- bin/roman_numerals.rb
+++ bin/roman_numerals.rb
@@ -1,5 +1,26 @@
 puts "Convert a number to roman numeral:"
-number = "V"
+number = gets.chomp.to_i
 
-puts number
+num_hash = {
+  1 => "I",
+  4 => "IV",
+  5 => "V",
+  9 => "IX",
+  10 =>	"X",
+  40 =>	"XL",
+  50 =>	"L",
+  90 =>	"XC",
+  100 => "C",
+  400 => "CD",
+  500 => "D",
+  900 => "CM",
+  1000 =>	"M"
+}
 
+num_hash.each do |k, v|
+  if number % k.to_i == 0
+    puts v
+  end
+end
+
+#Use modulo to see if it is divisible by 1 or 5
