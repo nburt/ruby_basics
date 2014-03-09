6ccc2017776242f97355ade3e57bf3f7202f8b11
Nathanael Burt <nathanael.burt@gmail.com>
2014-03-09 15:00:40 -0600

    Created method and conditional statements for double digit numbers

--- bin/roman_numerals.rb
+++ bin/roman_numerals.rb
@@ -1,26 +1,44 @@
 puts "Convert a number to roman numeral:"
 number = gets.chomp.to_i
 
-num_hash = {
-  1 => "I",
-  4 => "IV",
-  5 => "V",
-  9 => "IX",
-  10 =>	"X",
-  40 =>	"XL",
-  50 =>	"L",
-  90 =>	"XC",
-  100 => "C",
-  400 => "CD",
-  500 => "D",
-  900 => "CM",
-  1000 =>	"M"
-}
+def roman_numerals number
+  roman = ""
+  ones = number % 10
+  tens = number % 100 / 10
 
-num_hash.each do |k, v|
-  if number % k.to_i == 0
-    puts v
+  if tens == 9
+    roman = roman + "XC"
+  elsif tens == 4
+    roman = roman + "XL"
+  else
+    roman = roman + "L" * (number % 100 / 50)
+    roman = roman + "X" * (number % 50 / 10)
+  end
+
+  if ones == 9
+    roman = roman + "IX"
+  elsif ones == 4
+    roman = roman + "IV"
+  else
+    roman = roman + "V" * (number % 10 / 5)
+    roman = roman + "I" * (number % 5 / 1)
   end
 end
 
-#Use modulo to see if it is divisible by 1 or 5
+puts roman_numerals(number)
+
+=begin
+1	I
+4	IV
+5	V
+9	IX
+10	X
+40	XL
+50	L
+90	XC
+100	C
+400	CD
+500	D
+900	CM
+1000	M
+=end
